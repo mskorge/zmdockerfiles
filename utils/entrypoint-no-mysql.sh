5c281ca5f4940f1d47c2e33022e30e2b0bfbc99c
mskorge <github@skorge.se>
2019-02-02 21:48:31 +0100

    Debug CM start

--- utils/entrypoint-no-mysql.sh
+++ utils/entrypoint-no-mysql.sh
@@ -306,12 +306,12 @@ start_http () {
 # ZoneMinder service management
 start_zoneminder () {
     echo -n " * Starting ZoneMinder video surveillance recorder"
-    $ZMPKG start > /dev/null 2>&1
+    $ZMPKG start #> /dev/null 2>&1
     RETVAL=$?
     if [ "$RETVAL" = "0" ]; then
         echo "   ...done."
     else
-        echo "   ...failed!"
+        echo "   ...failed! ${$RETVAL}"
     fi
 }
 
